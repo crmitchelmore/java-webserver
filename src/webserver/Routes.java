fbc82b7a84abc8d32be4fef3aee8518206cd9be7
Chris Mitchelmore <chrismitchelmore@gmail.com>
2014-04-05 18:39:36 +0100

    Adds catch all for endswith routes

--- src/webserver/Routes.java
+++ src/webserver/Routes.java
@@ -14,9 +14,10 @@ public class Routes {
     public static JavaWebController route(String rootDirectory, String uri, HashMap<String, String> urlParams, HashMap<String, Object> postParams)
     {
         //This would usually route to scripts but as we are using java we route to classes!
-        if ( uri.equals("multipartform.ja") ){
+        //Also cheating slightly with ends with but I don't know where you will run the post_tests.html from!
+        if ( uri.endsWith("multipartform.ja") ){
             return new Profile(rootDirectory,urlParams, postParams);
-        }else if ( uri.equals("urlencodedform.ja") ){
+        }else if ( uri.endsWith("urlencodedform.ja") ){
             return new Lines(rootDirectory,urlParams, postParams);
         }
 
